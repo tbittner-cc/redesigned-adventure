efe744f04d18bf22f4c398e98475e58c45eb94db
Todd Bittner <tbittner@coastalchicago.com>
2024-07-08 18:47:03 -0500

    Finally determined how to save and reload images into and from a database.

--- resize_and_save_location_img.py
+++ resize_and_save_location_img.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import os
 import argparse
 from PIL import Image
@@ -18,13 +19,28 @@ print(f"Width: {width}, Height: {height}")
 print("Width-to-height ratio: {:.2f}".format(width / height))
 
 resized_image = image.resize((width // 5, height // 5))
-image_bytes = image.tobytes()
+img_byte_stream = BytesIO()
+
+resized_image.save(img_byte_stream, format="JPEG")
+
+image_bytes = img_byte_stream.getvalue()
+
+# new_image = Image.open(BytesIO(image_bytes))
+# new_image.show()
+
+# r_img_bytes = resized_image.tobytes()
+# new_image = Image.frombytes(resized_image.mode, resized_image.size, r_img_bytes)
+# new_image.show()
 
 with sqlite3.connect('travelectable.db') as conn:
-    curr = conn.cursor()
-    curr.execute("UPDATE destinations set image = ? WHERE id = ?", 
-        (sqlite3.Binary(image_bytes),args.location_id))
-    conn.commit()
-    #image = curr.fetchone()[0]
-    #image = Image.open(BytesIO(image))
-    #image.save(f"{args.location_id}.png")
+     curr = conn.cursor()
+     curr.execute("UPDATE destinations SET image = ? WHERE id = ?", (sqlite3.Binary(image_bytes), args.location_id))
+     conn.commit()
+
+with sqlite3.connect('travelectable.db') as conn:
+     curr = conn.cursor()
+     curr.execute("SELECT image FROM destinations WHERE id = ?", (args.location_id,))
+     rows = curr.fetchone()
+     image = rows[0]
+     image = Image.open(BytesIO(image))
+     image.show()
