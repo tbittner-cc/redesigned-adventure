1cc3f25f915029387b31de02bc92e6de363b2175
Todd Bittner <tbittner@coastalchicago.com>
2024-07-08 13:34:54 -0500

    Started code to save images to the db.

--- resize_and_save_location_img.py
+++ resize_and_save_location_img.py
@@ -1,8 +1,15 @@
 import os
+import argparse
 from PIL import Image
+import sqlite3
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_name", type=str)
+parser.add_argument("location_id", type=int)
+args = parser.parse_args()
 
 # Open an image file
-image = Image.open("chicago_original.jpg")
+image = Image.open(args.file_name)
 
 # Get the width and height of the image
 width, height = image.size
@@ -10,23 +17,14 @@ width, height = image.size
 print(f"Width: {width}, Height: {height}")
 print("Width-to-height ratio: {:.2f}".format(width / height))
 
-size_in_bytes = os.path.getsize("chicago_original.jpg")
+resized_image = image.resize((width // 5, height // 5))
+image_bytes = image.tobytes()
 
-size_in_mb = size_in_bytes / (1024 * 1024)
-
-print(f"Size in MB: {size_in_mb:.2f}MB")
-
-resized_image = image.resize((width // 5,int(width // 5*(height/width))))
-
-resized_image.save("chicago_resized.jpg")
-
-width, height = resized_image.size
-
-print(f"Width: {width}, Height: {height}")
-print("Width-to-height ratio: {:.2f}".format(width / height))
-
-size_in_bytes = os.path.getsize("chicago_resized.jpg")
-
-size_in_mb = size_in_bytes / (1024 * 1024)
-
-print(f"Size in MB: {size_in_mb:.2f}MB")
+with sqlite3.connect('travelectable.db') as conn:
+    curr = conn.cursor()
+    curr.execute("UPDATE destinations set image = ? WHERE id = ?", 
+        (sqlite3.Binary(image_bytes),args.location_id))
+    conn.commit()
+    #image = curr.fetchone()[0]
+    #image = Image.open(BytesIO(image))
+    #image.save(f"{args.location_id}.png")
